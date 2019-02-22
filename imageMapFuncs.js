91e35169c37ba10e0e01a5f1b6f5938c4c32cf82
Swi005 <sander.wiig@gmail.com>
2019-02-22 17:08:31 +0100

    rewrote imageMapFuncs.js'

--- imageMapFuncs.js
+++ imageMapFuncs.js
@@ -1,6 +1,12 @@
-function calculateRatio(X1, Y1, X2, Y2){
-    return [X1/X2, Y1/Y2];
+function addNewArea(areaArr, Ximg, Yimg, Xcanvas, Ycanvas, x1, y1, x2, y2){
+    
+    var ratioX = Ximg / Xcanvas;
+    var ratioY = Yimg / Ycanvas;
+
+    x1 *= ratioX;
+    x2 *= ratioX;
+    y1 *= ratioY;
+    y2 *= ratioY;
+    areaArr.push(`<area shape="rect" coords="{x1} , {y1}, {x2}, {y2}" href="#">`);
+
 }
-function translateMouseToReal(X, Y, RX, RY){
-    return [X*RX, Y*RY];
-}    
