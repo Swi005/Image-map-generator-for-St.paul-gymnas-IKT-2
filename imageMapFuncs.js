735ed3c3d596aefa0f8cf505434720cffbabeef6
Swi005 <sander.wiig@gmail.com>
2019-02-22 17:13:53 +0100

    optimized and added more comments

--- imageMapFuncs.js
+++ imageMapFuncs.js
@@ -1,12 +1,12 @@
 function addNewArea(areaArr, Ximg, Yimg, Xcanvas, Ycanvas, x1, y1, x2, y2){
+    //function for adding a new area to image map, 
     
+    //calculate ratioes
     var ratioX = Ximg / Xcanvas;
     var ratioY = Yimg / Ycanvas;
-
-    x1 *= ratioX;
-    x2 *= ratioX;
-    y1 *= ratioY;
-    y2 *= ratioY;
+    
+    //adjust coords
+    x1, x2 *= ratioX;
+    y1, y2 *= ratioY;
     areaArr.push(`<area shape="rect" coords="{x1} , {y1}, {x2}, {y2}" href="#">`);
-
 }
